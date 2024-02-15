a80ce933925b3a6ec2e6b663046cf87b6be750fc
Bett <bettpatrick080@gmail.com>
2024-02-15 22:05:12 +0300

    Functional Game component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+import axios from 'axios';
 
 const Search = () => {
-  
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(`http://localhost:3000/games`);
+      setSearchResults(response.data); // Assuming the API response contains the search results
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
-    <div className="search">
-        <form className='search-form' action="">
-            <input type="text" placeholder="Search"/>
-            <input type="submit" value="Search"/>
+    <div>
+      <div className="search">
+        <form className='search-form' onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <input type="submit"/>
         </form>
-    </div>
-
-  )
-}
-
+        </div>
+    </div>)}
 
 export default Search;
--- src/components/PopulatedGames.jsx
+++ src/components/PopulatedGames.jsx
@@ -17,7 +17,7 @@ const PopulatedGames = ({ games, clickedGenre }) => {
             <img src={game.thumbnail} alt={game.title} />
             <h3>{game.title}</h3>
 
-            <Link to="/game">
+            <Link to={`/game/${game.id}`}>
               <button onClick={()=>{handleViewClick}}>View</button>
               </Link>
           </div>    
