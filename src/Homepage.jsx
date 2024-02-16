eeb17b44eb844342509dc4f8a380d9f8cca85068
Bett <bettpatrick080@gmail.com>
2024-02-16 03:01:03 +0300

    Displayed default games when no genre clicked

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import SideBar from './SideBar';
 import NavBar from './NavBar';
@@ -6,43 +6,45 @@ import Footer from './Footer';
 import PopularGames from './components/PopularGames/PopularGames';
 import PopulatedGames from './components/PopulatedGames';
 
-
-
 const Homepage = () => {
-  const [games, setGames] = useState([])
-  const [clickedGenre, setClickedGenre]=useState(null)
+  const [games, setGames] = useState([]);
+  const [clickedGenre, setClickedGenre] = useState(null);
 
-  const handleGenreClick=(genre)=>{
-    setClickedGenre(genre)
-  }
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:3000/games')
-      .then(res=>res.json())
-      .then((games)=>{
-        console.log(games)
-        setGames(games)
-      } )
-  }, [])
+      .then(res => res.json())
+      .then(games => {
+        setGames(games);
+      })
+      .catch(error => {
+        console.error('Error fetching games:', error);
+      });
+  }, []);
 
-  const handleViewClick = (id)=> {
-    alert(id)
-  }
+  const handleGenreClick = genre => {
+    setClickedGenre(genre);
+  };
 
   return (
     <div className='App'>
       <div className='aside-plus-nav'>
-        <SideBar games={games} onGenreClick={handleGenreClick}/>
-    
-       <div className='nav-container'>
-        <NavBar/>
-        {PopulatedGames? 
-        <PopulatedGames games={games} clickedGenre={clickedGenre} handleViewClick = {handleGenreClick} />
-        :<PopularGames games={games}/>}
-       </div>
+        <SideBar games={games} onGenreClick={handleGenreClick} />
+        <div className='nav-container'>
+          <NavBar />
+          {/* Render PopularGames component when no genre is clicked */}
+          {!clickedGenre && <PopularGames games={games} />}
+          {/* Render PopulatedGames component when a genre is clicked */}
+          {clickedGenre && (
+            <PopulatedGames
+              games={games}
+              clickedGenre={clickedGenre}
+            />
+          )}
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
--- src/components/PopularGames/PopularGames.jsx
+++ src/components/PopularGames/PopularGames.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import '../../../src/index.css'
+import React from 'react';
 
+const PopularGames = ({ games, clickedGenre }) => {
+  // Check if no genre is clicked
+  if (!clickedGenre) {
+    return (
+      <div className='popular-games'>
+        {games.map(game => (
+          <div key={game.id}>
+            <img src={game.thumbnail} alt={game.title} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-
-const PopularGames = ({games}) => {
-    
-  return (
-    <div className='popular-games'>
-        <div>
-            <img src={games.thumbnail} alt={games.title} />
-        </div>
-    </div>
-  )
+  // Render nothing if a genre is clicked
+  return null;
 }
 
-export default PopularGames
+export default PopularGames;
